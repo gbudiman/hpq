82a46cf3e8f3bee01744b31faea84fbb34d39ef9
Gloria Budiman <wahyu.g@gmail.com>
2017-04-05 18:14:04 -0700

    Initial project setup

---
+++ hpq/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hpq
+//
+//  Created by Gloria Budiman on 4/5/17.
+//  Copyright © 2017 gbudiman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
